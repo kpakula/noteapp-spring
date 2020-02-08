e8ef0392eac98e656006f917daa46155d3f5d033
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-02-08 18:48:22 +0100

    Handled user sign in

--- src/main/java/com/example/project/controllers/AUserController.java
+++ src/main/java/com/example/project/controllers/AUserController.java
@@ -2,6 +2,7 @@ package com.example.project.controllers;
 
 import com.example.project.exceptions.AUserNotFoundException;
 import com.example.project.model.AUser;
+import com.example.project.model.LoginModel;
 import com.example.project.repository.AUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +20,13 @@ public class AUserController {
     @GetMapping("/users")
     List<AUser> all() {
         return AUserRepository.findAll();
+    }
+
+    @CrossOrigin
+    @PostMapping("/signin")
+    @ResponseBody
+    public Boolean login(@RequestBody LoginModel model) {
+        return AUserRepository.existsByLoginAndPassword(model.getLogin(), model.getPassword());
     }
 
 
